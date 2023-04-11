57ceaea1e1ab08509ef7ded4e1cd157560be1aed
biduzilla <rlyowlly19@gmail.com>
2023-04-11 17:51:49 -0300

    Criação da tela

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Primeira Tela'),
+          title: Text('Tarefas'),
         ),
         body: Container(
-          color: Colors.green,
+          child: Stack(
+            children: [
+              Container(color: Colors.blue, height: 140),
+              Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Text('Aprender Flutter'),
+                    ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: (){}),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
       ),
     );
   }
