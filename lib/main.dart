9cec6aa5bcd3c502175c8e46174587590ae5a7e2
biduzilla <rlyowlly19@gmail.com>
2023-04-12 09:59:37 -0300

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,17 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
 
   const Task(this.nome, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -50,29 +57,47 @@ class Task extends StatelessWidget {
         child: Stack(
           children: [
             Container(color: Colors.blue, height: 140),
-            Container(
-              color: Colors.white,
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.black26,
-                    width: 72,
-                    height: 100,
+            Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                      ),
+                      Container(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          )),
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              nivel++;
+                              print(nivel);
+                            });
+                          },
+                          child: const Icon(Icons.arrow_drop_up))
+                    ],
                   ),
-                  Container(
-                      width: 200,
-                      child: Text(
-                        nome,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow.ellipsis,
-                      )),
-                  ElevatedButton(
-                      onPressed: () {}, child: const Icon(Icons.arrow_drop_up))
-                ],
-              ),
+                ),
+                Text(
+                  'Nível $nivel',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                )
+              ],
             ),
           ],
         ),
